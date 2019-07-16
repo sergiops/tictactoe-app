12306eb0eec124d05f45bcee46cf6ff21cdf97ac
Sergio Panjoj <sergiopanjoj@gmail.com>
2019-07-16 12:50:51 -0700

    Initial Commit

---
+++ tictactoe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tictactoe
+//
+//  Created by Sergio P. on 7/16/19.
+//  Copyright © 2019 Sergio P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
