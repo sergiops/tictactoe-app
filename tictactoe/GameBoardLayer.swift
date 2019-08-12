c49e75727c5f8f316ef4b976f26d623795748810
Sergio Panjoj <sergiopanjoj@gmail.com>
2019-08-12 01:10:41 -0700

    seperate sublayers into subclasses

---
+++ tictactoe/GameBoardLayer.swift
@@ -0,0 +1,13 @@
+//
+//  GameBoardLayer.swift
+//  tictactoe
+//
+//  Created by Sergio P. on 8/11/19.
+//  Copyright © 2019 Sergio P. All rights reserved.
+//
+
+import UIKit
+
+class GameBoardLayer: CALayer {
+
+}
---
+++ tictactoe/MessageLayer.swift
@@ -0,0 +1,13 @@
+//
+//  MessageLayer.swift
+//  tictactoe
+//
+//  Created by Sergio P. on 8/11/19.
+//  Copyright © 2019 Sergio P. All rights reserved.
+//
+
+import UIKit
+
+class MessageLayer: CALayer {
+
+}
