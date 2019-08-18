b403fd17f880dac275e67f6eeaacfc459a88ad2a
Sergio Panjoj <sergiopanjoj@gmail.com>
2019-08-18 15:00:42 -0700

    add animations when showing message layer

--- tictactoe/MessageLayer.swift
+++ tictactoe/MessageLayer.swift
@@ -11,18 +11,30 @@ import UIKit
 class MessageLayer: CALayer {
     let fadeDuration: Double = 0.2
     let translateDuration: Double = 0.5
+    
+    public func moveInBottom(message: String,
+                             messageRect: CGRect,
+                             icon: CAShapeLayer) {
 
+        let text = createTextLayer(string: message, rect: messageRect)
+        let animatedText = addMoveInBottomAnimation(layer: text) as! CATextLayer
+        let animatedIcon = addMoveInBottomAnimation(layer: icon) as! CAShapeLayer
+        
+        self.addSublayer(animatedIcon)
+        self.addSublayer(animatedText)
+    }
+    
     public func showGameOverMessage(message: String,
                                     messageRect: CGRect,
                                     icon: CAShapeLayer,
                                     iconOffset: CGPoint) {
+        let iconAnimation = getTranslationAnimation(translateDuration, iconOffset)
+        icon.add(iconAnimation, forKey: "translateCenter")
+        var text = createTextLayer(string: message, rect: messageRect)
+        text = addMoveInBottomAnimation(layer: text) as! CATextLayer
         
-        let animation = getTranslationAnimation(translateDuration, iconOffset)
-        icon.add(animation, forKey: "translateCenter")
-        
-        let message = createTextLayer(string: message, rect: messageRect)
         self.addSublayer(icon)
-        self.addSublayer(message)
+        self.addSublayer(text)
     }
     
     // Hide the message from the user's view.
@@ -40,11 +52,13 @@ class MessageLayer: CALayer {
         CATransaction.commit()
     }
     
+    // Return a text layer with the specified string.
     private func createTextLayer(string: String, rect: CGRect) -> CATextLayer {
         let textLayer = CATextLayer()
         textLayer.frame = rect
         textLayer.string = string
-        textLayer.font = UIFont.boldSystemFont(ofSize: CGFloat(20.0))
+        textLayer.font = UIFont.systemFont(ofSize: CGFloat(20.0),
+                                           weight: UIFont.Weight.heavy)
         textLayer.foregroundColor = UIColor.white.cgColor
         textLayer.contentsScale = UIScreen.main.scale
         textLayer.isWrapped = true
@@ -73,6 +87,27 @@ class MessageLayer: CALayer {
         animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
         animation.beginTime = CACurrentMediaTime() + self.fadeDuration
         return animation
+    }
+    
+    private func addMoveInBottomAnimation(layer: CALayer) -> CALayer {
+        let moveIn = getSlideInFadeInAnimation(offsetY: 75)
+        layer.add(moveIn, forKey: "moveInBottom")
+        return layer
+    }
+    
+    private func getSlideInFadeInAnimation(offsetY: Int) -> CAAnimationGroup {
+        let fadeIn = CABasicAnimation(keyPath: "opacity")
+        fadeIn.fromValue = 0
+        fadeIn.toValue = 1
+        
+        let translation = CABasicAnimation(keyPath: "transform.translation.y")
+        translation.fromValue = offsetY
+        
+        let group = CAAnimationGroup()
+        group.animations = [fadeIn, translation]
+        group.duration = 0.75
+        group.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        return group
     }
     
 }
